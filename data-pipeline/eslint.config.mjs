b717ada3e76eebb12c1dc3bc5a75dbc902ad292d
Hubert Lee 🤖 <hube@users.noreply.github.com>
2026-05-11 11:50:53 -0700

    Use current Yarn stable version, upgrade dependencies, and switch to @tsconfig/node22

--- data-pipeline/eslint.config.mjs
+++ data-pipeline/eslint.config.mjs
@@ -5,5 +5,5 @@ import tseslint from 'typescript-eslint';
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  globalIgnores(["dist"])
+  globalIgnores(["dist", ".pnp.*", ".yarn/**"])
 );
