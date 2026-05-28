ef6bb62b17216d93ab88332f391312595adce8d3
Hubert Lee 🤖 <hube@users.noreply.github.com>
2026-05-27 22:54:44 -0700

    Add devcontainer configuration

---
+++ .devcontainer/local-features/aws-cdk/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install the AWS CDK. Instructions originally copied from
+# https://docs.aws.amazon.com/cdk/v2/guide/getting-started.html
+
+# # Needed because NPM isn't in the PATH
+# \. "$HOME/.nvm/nvm.sh"
+
+npm install -g aws-cdk
